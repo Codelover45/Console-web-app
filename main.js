79973791cf042d12833c18295c72e0557db36a33
Codelover45 <85251886+Codelover45@users.noreply.github.com>
2022-01-09 12:36:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Durga";
+var last_name = "Mohan";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a= 10;
+var b= 5;
+
+var add = a + b ; 
+console.log(add);
